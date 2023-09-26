6b609a687eea5b6c5fafe01006c9160c0df2bde0
unknown <tonidelacalle@gmail.com>
2023-09-26 17:13:55 +0200

    Debouncing on mousemove. Wip

--- src/components/TheMap.vue
+++ src/components/TheMap.vue
@@ -11,7 +11,7 @@
 
 <script>
 import { Map, Popup, NavigationControl } from "maplibre-gl";
-import { shallowRef, onMounted, onUnmounted, markRaw } from "vue";
+import { shallowRef, onMounted, onUnmounted, markRaw, handleError } from "vue";
 import { mapData } from "src/assets/geojson.js";
 import { api } from "src/boot/axios";
 
@@ -20,6 +20,7 @@ export default {
   setup() {
     const mapContainer = shallowRef(null);
     const map = shallowRef(null);
+    let popup;
 
     const COUNTRY_PALETTE = [
       "#f0d27e",
@@ -51,15 +52,18 @@ export default {
       map.value.once("load", () => {
         // This code runs once the base style has finished loading.
         const name_expr = ["get", "name"];
+        var tesisUrl = process.env.DEV
+          ? "/tesis_totals.json"
+          : "//sigserver4.udg.edu/tesis/spa/tesis_totals.json";
+
         api
           .get(
             "//d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson"
           )
           .then((response) => {
             countriesData = response.data;
-            console.log(countriesData);
             api
-              .get("/tesis_totals.json")
+              .get(tesisUrl)
               .then((resp) => {
                 tesisData = resp.data;
                 countriesData.features.forEach((element, idx) => {
@@ -77,17 +81,12 @@ export default {
       });
 
       // Create a popup, but don't add it to the map yet.
-      const popup = new Popup({
+      popup = new Popup({
         closeButton: false,
         closeOnClick: false,
       });
 
-      map.value.on("mousemove", "countries-polygon", (e) => {
-        map.value.getCanvas().style.cursor = "pointer";
-        var content =
-          e.features[0].properties.name + ": " + e.features[0].properties.tesis;
-        popup.setLngLat(e.lngLat).setHTML(content).addTo(map.value);
-      });
+      map.value.on("mousemove", "countries-polygon", debounce);
 
       map.value.on("mouseleave", "countries-polygon", (e) => {
         map.value.getCanvas().style.cursor = "";
@@ -99,7 +98,6 @@ export default {
       });
 
     function addCountriesLayer(data) {
-      console.log(data);
       map.value.addSource("countries", {
         type: "geojson",
         data: data,
@@ -130,6 +128,74 @@ export default {
           "fill-opacity": 0.8,
         },
       });
+    }
+
+    function debounce(e) {
+      let timer;
+      console.log(e.features);
+      clearTimeout(timer);
+      timer = setTimeout(handleMouseMove, 1000, e);
+    }
+
+    function handleMouseMove(e) {
+      console.log("Inside");
+      console.log(e);
+      var color1 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      var color2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      var color3 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      var color4 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      var color5 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      var color6 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+      var randomColors = [
+        "case",
+        [">", ["to-number", ["get", "tesis"]], 1000],
+        color1,
+        [">", ["to-number", ["get", "tesis"]], 30],
+        color2,
+        [">=", ["to-number", ["get", "tesis"]], 5],
+        color3,
+        [">=", ["to-number", ["get", "tesis"]], 4],
+        color4,
+        [">=", ["to-number", ["get", "tesis"]], 2],
+        color5,
+        ["==", ["to-number", ["get", "tesis"]], 1],
+        color6,
+        "#fff0",
+      ];
+
+      map.value.getCanvas().style.cursor = "pointer";
+      var content =
+        e.features[0].properties.name + ": " + e.features[0].properties.tesis;
+
+      if (e.features[0].properties.tesis > 50) {
+        map.value.setPaintProperty("countries-polygon", "fill-color", [
+          "case",
+          [">", ["to-number", ["get", "tesis"]], 1000],
+          "violet",
+          [">", ["to-number", ["get", "tesis"]], 30],
+          "red",
+          [">=", ["to-number", ["get", "tesis"]], 5],
+          "orange",
+          [">=", ["to-number", ["get", "tesis"]], 4],
+          "yellow",
+          [">=", ["to-number", ["get", "tesis"]], 2],
+          "green",
+          [">=", ["to-number", ["get", "tesis"]], 1],
+          "blue",
+          "#fff0",
+        ]);
+      } else {
+        map.value.setPaintProperty(
+          "countries-polygon",
+          "fill-color",
+          randomColors
+        );
+      }
+
+      if (e.features[0].properties.tesis) {
+        popup.setLngLat(e.lngLat).setHTML(content).addTo(map.value);
+      }
     }
 
     return {
